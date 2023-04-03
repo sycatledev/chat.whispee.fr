35cd8a628cd113a4b2379bab0267d1ee408eedd4
Ali <121733733+siki72@users.noreply.github.com>
2023-04-03 15:24:10 +0200

    PM-13: Create React app and Vite

---
+++ OldClient/frontEnd/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+function App() {
+
+
+  return (
+      <div>
+          <section>
+              // Form container
+              <div>
+                  <form>
+
+                          <label></label>
+                          <input/>
+
+
+                          <label></label>
+                          <input/>
+
+
+                  </form>
+              </div>
+              // Infos container
+              <div></div>
+          </section>
+
+
+
+          <footer></footer>
+      </div>
+
+  )
+}
+
+export default App
