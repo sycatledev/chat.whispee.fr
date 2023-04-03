4d5ac82d9f66883a113d8deb0610c1ffa89ddb83
Ali <121733733+siki72@users.noreply.github.com>
2023-04-03 16:46:58 +0200

    PM-13: implemeted login page and footer layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,39 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
   return (
     <div>
-      <h2>footer</h2>
+      <h3>Private message up by Sycatle</h3>
+      <section>
+        <ul>
+          <li>USEFUL LINKS</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>Blog</li>
+        </ul>
+        <ul>
+          <li>Contacts</li>
+          <li>
+            <address>1234, Code avenue, France</address>
+          </li>
+          <FontAwesomeIcon icon={faFacebook} />{" "}
+          <FontAwesomeIcon icon={faTwitter} />{" "}
+          <FontAwesomeIcon icon={faInstagram} />{" "}
+          <FontAwesomeIcon icon={faGithub} />{" "}
+        </ul>
+        <div>
+          <label htmlFor="sub">NEWS LETTER</label>
+          <input type="text" id="sub" placeholder="Your Email Adress" />
+          <button>SUBSCRIBE NOW</button>
+        </div>
+      </section>
     </div>
   );
 };
--- client/src/pages/Authentification.jsx
+++ client/src/pages/Authentification.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 const Authentification = () => {
   return (
     <div>
-      <h1>auth page</h1>
+      <section>
+        <div>
+          <h2>Sign In</h2>
+          <form action="" method="post">
+            <label htmlFor="identifier">
+              {" "}
+              <FontAwesomeIcon icon={faUser} />{" "}
+            </label>
+            <input type="text" id="identifier" placeholder="Identifiant" />
+
+            <label htmlFor="">
+              <FontAwesomeIcon icon={faLock} />
+            </label>
+            <input type="password" name="" id="" placeholder="Password" />
+            <label htmlFor="username">
+              {" "}
+              <FontAwesomeIcon icon={faUser} />{" "}
+            </label>
+            <input type="text" id="username" placeholder="Username" />
+            <label htmlFor="email">
+              {" "}
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+            </label>
+            <input type="email" id="email" placeholder="Email" />
+
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div>
+          <h1>Private Message App</h1>
+          <p>Coming soon , App under construction</p>
+        </div>
+      </section>
     </div>
   );
 };
