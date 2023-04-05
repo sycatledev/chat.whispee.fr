257e19e1272b36c757f2691f7953bf0894a1f789
Ali <121733733+siki72@users.noreply.github.com>
2023-04-05 15:19:32 +0200

    PM-23: fixed double chat message's issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,4 @@ import App from "./App";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import functions from "../utils/functions.js";
 import * as Utils from "../utils/generator.js";
+import * as Theme from "../utils/theme.js";
 
 const Chat = () => {
   const [websocket, setWebsocket] = useState(null);
@@ -8,36 +8,32 @@ const Chat = () => {
   const [chatsContainer, setChatsContainer] = useState(null);
   const [chats, setChats] = useState([]);
   const [ready, setReady] = useState(false);
+  const [singlChat, setSingleChat] = useState(false);
+  const [parsedMessages, setParsedMessages] = useState([]);
+  const [inputDatas, setInputDatas] = useState("");
 
   useEffect(() => {
     const init = async () => {
       const ws = new WebSocket("ws://localhost:456/");
       ws.addEventListener("open", async (event) => {
         console.log("Connected to server");
-
         if (currentChat != null) {
           let request = { chat_id: currentChat };
           await sendSocketMessage(ws, "load_chat|||" + JSON.stringify(request));
         }
-
         await sendSocketMessage(ws, "load_chats|||");
-
         setWebsocket(ws);
       });
-
       ws.addEventListener("close", async (event) => {
         console.log("Lost connection to server");
       });
-
       ws.addEventListener("error", async (event) => {
         console.error("Websocket error", event);
       });
-
       ws.addEventListener("message", async (event) => {
         await handleSocketMessage(event.data);
       });
     };
-
     init();
   }, [currentChat]);
 
@@ -52,26 +48,17 @@ const Chat = () => {
 
     if (socketCommand === "chat_message_sended") {
       let messageData = JSON.parse(socketData);
-
-      createChatMessageElement(
-        messageData.message_uuid,
-        messageData.message_content
-      );
+      setParsedMessages((parsedMessages) => [...parsedMessages, messageData]);
+      setReady(true);
     } else if (socketCommand === "loaded_chats") {
       let chatsData = JSON.parse(socketData);
       setChats(chatsData);
       setReady(true);
-      for (let index of chats) {
-        let chat = chats[index];
-        /*         createChatListElement(
-          chat.chat_id,
-          chat.chat_name,
-          chat.chat_pending_messages
-        ); */
-      }
     } else if (socketCommand === "chat_loaded") {
       let chat_data = JSON.parse(socketData);
-      await updateChatElement(chat_data);
+      setChatsContainer(chat_data);
+      setParsedMessages(JSON.parse(chat_data.chat_messages));
+      setSingleChat(true);
     }
   };
 
@@ -83,7 +70,6 @@ const Chat = () => {
 
   const loadChat = async (chat_id) => {
     if (currentChat === chat_id) return;
-
     setCurrentChat(chat_id);
 
     let request = { chat_id: currentChat };
@@ -105,136 +91,15 @@ const Chat = () => {
       `send_chat_message|||${JSON.stringify(data)}`
     );
   };
-  async function updateChatElement(chat) {
-    console.log(chat);
-    let firstNameLetter = chat.chat_name[0];
-    let chatHeader = document.getElementById("chat-header");
-    let messagesContainer = document.getElementById("messages-container");
-    let chatForm = document.getElementById("chat-form");
 
-    let messages = JSON.parse(chat.chat_messages);
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    if (inputDatas.length < 1) return;
+    if (inputDatas.trim() === "") return;
+    setInputDatas("");
+    sendChatMessage(currentChat, inputDatas);
+  };
 
-    chatHeader.innerHTML = `
-      <div class="flex-grow cursor-pointer">
-          <div class="flex flex-row items-center relative w-full">
-              <div class="flex items-center justify-center h-8 w-8 bg-indigo-400 text-white rounded-full">
-                  ${firstNameLetter}
-              </div>
-              <div class="ml-2 text-sm font-semibold">
-                  ${chat.chat_name}
-              </div>
-          </div>
-      </div>
-      <button class="flex items-center justify-center text-gray-500 hover:text-gray-800 active:bg-indigo-200 dark:hover:text-gray-300 dark:active:bg-indigo-400 p-2 rounded-xl flex-shrink-0">
-          <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-              <path d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z">
-              </path>
-              <path d="M12 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z">
-              </path>
-              <path d="M12 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z">
-              </path>
-          </svg>
-      </button>`;
-
-    messagesContainer.innerHTML = ``;
-
-    for (let message in messages) {
-      messagesContainer.insertAdjacentHTML(
-        "beforeend",
-        `
-          <div id="${messages[message].message_uuid}" class="col-start-9 col-end-13 p-3 rounded-lg">
-              <div class="flex items-center justify-start flex-row-reverse">
-                  <div class="flex items-center justify-center h-10 w-10 rounded-full text-white bg-indigo-400 flex-shrink-0">
-                      A
-                  </div>
-                  <div class="relative mr-3 text-sm bg-white text-black dark:bg-black dark:text-white py-2 px-4 shadow rounded-xl">
-                      <div>${messages[message].message_content}</div>
-                  </div>
-              </div>
-          </div>
-      `
-      );
-    }
-
-    chatForm.innerHTML = `<div class="flex-grow">
-          <div class="relative w-full">
-          <input
-              type="text"
-              autocomplete="off"
-              placeholder="Send a message to ${chat.chat_name}"
-              class="flex w-full border rounded-xl bg-[#fefefe] dark:bg-[#121212] focus:outline-none focus:border-indigo-300 px-4 h-10"
-              id="chat-input"
-          />
-          </div>
-      </div>
-      <button
-          class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 rounded-xl text-white ml-1 px-4 py-2 flex-shrink-0"
-      >
-          <svg
-          class="w-5 h-5 rotate-45"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          >
-          <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-          ></path>
-          </svg>
-      </button>`;
-
-    let input = document.getElementById("chat-input");
-
-    chatForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      let message = input.value;
-      console.log(message);
-
-      if (message.length < 1) return;
-      if (message.trim() === "") return;
-
-      input.value = "";
-      sendChatMessage(currentChat, message);
-    });
-  }
-
-  async function createChatMessageElement(uuid, content) {
-    // <div class="col-start-1 col-end-8 p-3 rounded-lg">
-    //     <div class="flex flex-row items-center">
-    //         <img src="https://api.lorem.space/image/face?w=150&h=150" class="flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full">
-    //         <div class="relative ml-3 text-sm bg-white dark:bg-black text-black dark:text-white duration-300 py-2 px-4 shadow rounded-xl">
-    //         <div>
-    //             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae unde aliquid, laboriosam provident reiciendis autem?
-    //         </div>
-    //         </div>
-    //     </div>
-    // </div>
-
-    let messagesContainer = document.getElementById("messages-container");
-
-    messagesContainer.insertAdjacentHTML(
-      "beforeend",
-      `
-          <div id="${uuid}" class="col-start-6 col-end-13 p-3 rounded-lg">
-              <div class="flex items-center justify-start flex-row-reverse">
-                  <div class="flex items-center justify-center h-10 w-10 rounded-full text-white bg-indigo-400 flex-shrink-0">
-                      A
-                  </div>
-                  <div class="relative mr-3 text-sm bg-white text-black dark:bg-black dark:text-white py-2 px-4 shadow rounded-xl">
-                      <div>${content}</div>
-                  </div>
-              </div>
-          </div>
-      `
-    );
-
-    const newMessage = document.getElementById(uuid);
-    newMessage.scrollIntoView();
-  }
   return (
     <div className="bg-[#fefefe] dark:bg-[#080808] text-black dark:text-white duration-500">
       <div className="flex h-screen antialiased text-gray-800">
@@ -366,25 +231,128 @@ const Chat = () => {
               id="chat-wrapper"
               className="flex flex-col flex-auto flex-shrink-0 lg:rounded-2xl h-full bg-gradient-to-tl from-[#f8f6ff] to-[#e9e2ff] dark:from-[#2e2e2e] dark:to-black"
             >
-              <div
-                id="chat-header"
-                className="flex flex-row items-center h-16 lg:rounded-t-xl bg-[#f7f7f7] dark:bg-[#1c1c1c] text-black dark:text-white duration-300 w-full px-2"
-              ></div>
-              <div
-                id="chat-content"
-                className="flex flex-col h-full overflow-x-auto p-4 my-2"
-              >
-                <div className="flex flex-col h-full rounded-2xl">
+              {singlChat ? (
+                <>
                   <div
-                    className="grid grid-cols-12 gap-y-2"
-                    id="messages-container"
+                    id="chat-header"
+                    className="flex flex-row items-center h-16 lg:rounded-t-xl bg-[#f7f7f7] dark:bg-[#1c1c1c] text-black dark:text-white duration-300 w-full px-2"
+                  >
+                    <div className="flex-grow cursor-pointer">
+                      <div className="flex flex-row items-center relative w-full">
+                        <div className="flex items-center justify-center h-8 w-8 bg-indigo-400 text-white rounded-full">
+                          {ready && chatsContainer?.chat_name[0]}
+                        </div>
+                        <div className="ml-2 text-sm font-semibold">
+                          {ready && chatsContainer?.chat_name}
+                        </div>
+                      </div>
+                    </div>
+                    <button className="flex items-center justify-center text-gray-500 hover:text-gray-800 active:bg-indigo-200 dark:hover:text-gray-300 dark:active:bg-indigo-400 p-2 rounded-xl flex-shrink-0">
+                      <svg
+                        className="w-5 h-5"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+                        <path d="M12 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+                        <path d="M12 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+                      </svg>
+                    </button>
+                  </div>
+                  <div
+                    id="chat-content"
+                    className="flex flex-col h-full overflow-x-auto p-4 my-2"
+                  >
+                    <div className="flex flex-col h-full rounded-2xl">
+                      <div
+                        className="grid grid-cols-12 gap-y-2"
+                        id="messages-container"
+                      >
+                        {parsedMessages.map((message, index) => (
+                          <div
+                            key={index}
+                            id={message.message_uuid}
+                            className="col-start-9 col-end-13 p-3 rounded-lg"
+                          >
+                            <div className="flex items-center justify-start flex-row-reverse">
+                              <div className="flex items-center justify-center h-10 w-10 rounded-full text-white bg-indigo-400 flex-shrink-0">
+                                A
+                              </div>
+                              <div className="relative mr-3 text-sm bg-white text-black dark:bg-black dark:text-white py-2 px-4 shadow rounded-xl">
+                                <div>{message.message_content}</div>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <form
+                    onSubmit={handleSubmitForm}
+                    id="chat-form"
+                    className="flex flex-row items-center h-16 p-4 lg:rounded-b-xl bg-[#f7f7f7] dark:bg-[#1c1c1c] text-black dark:text-white duration-300 w-full px-4"
+                  >
+                    <div className="flex-grow">
+                      <div className="relative w-full">
+                        <input
+                          value={inputDatas}
+                          onChange={(e) => setInputDatas(e.target.value)}
+                          type="text"
+                          autoComplete="off"
+                          placeholder={`Send a message to ${chatsContainer?.chat_name}`}
+                          className="flex w-full border rounded-xl bg-[#fefefe] dark:bg-[#121212] focus:outline-none focus:border-indigo-300 px-4 h-10"
+                          id="chat-input"
+                        />
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 rounded-xl text-white ml-1 px-4 py-2 flex-shrink-0"
+                    >
+                      <svg
+                        className="w-5 h-5 rotate-45"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                        ></path>
+                      </svg>
+                    </button>
+                  </form>
+                </>
+              ) : (
+                <>
+                  <div
+                    id="chat-header"
+                    className="flex flex-row items-center h-16 lg:rounded-t-xl bg-[#f7f7f7] dark:bg-[#1c1c1c] text-black dark:text-white duration-300 w-full px-2"
                   ></div>
-                </div>
-              </div>
-              <form
-                id="chat-form"
-                className="flex flex-row items-center h-16 p-4 lg:rounded-b-xl bg-[#f7f7f7] dark:bg-[#1c1c1c] text-black dark:text-white duration-300 w-full px-4"
-              ></form>
+                  <div
+                    id="chat-content"
+                    className="flex flex-col h-full overflow-x-auto p-4 my-2"
+                  >
+                    <div className="flex flex-col h-full rounded-2xl">
+                      <div
+                        className="grid grid-cols-12 gap-y-2"
+                        id="messages-container"
+                      ></div>
+                    </div>
+                    <form
+                      id="chat-form"
+                      className="flex flex-row items-center h-16 p-4 lg:rounded-b-xl bg-[#f7f7f7] dark:bg-[#1c1c1c] text-black dark:text-white duration-300 w-full px-4"
+                    ></form>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
