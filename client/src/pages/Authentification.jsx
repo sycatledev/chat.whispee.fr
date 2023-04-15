2548599bf133b88e51da7daff8501682083573d5
sycatle <sycatle@pm.me>
2023-04-16 01:43:30 +0200

    fixed login page missing background

--- client/src/pages/Authentification.jsx
+++ client/src/pages/Authentification.jsx
@@ -76,7 +76,7 @@ const Authentification = () => {
   };
 
   return (
-    <div className="font-karla">
+    <div className="font-karla bg-gradient-to-br from-[#f8f6ff] to-[#ab94fd]">
       <div className="flex min-h-screen justify-center sm:items-center">
         <section className="grid grid-cols-12 sm:space-y-10">
           <div className="col-span-12 lg:col-span-7 font-bold m-auto text-center">
