3f768d3f0f2c8f044cda092a0c56d0c218bc483e
sycatle <sycatle@pm.me>
2023-05-03 01:06:14 +0200

    added Avatar component

---
+++ client/src/components/user/Avatar.jsx
@@ -0,0 +1,12 @@
+export default function Avatar({ username, size, color }) {
+  return (
+    <div
+      className={`flex items-center justify-center uppercase select-none
+      ${color || "bg-indigo-400"}
+      ${size ? `h-${size} w-${size}` : "h-8 w-8"} 
+      text-white rounded-full cursor-pointer hover:shadow group-hover:shadow`}
+    >
+      {username[0]}
+    </div>
+  );
+}
