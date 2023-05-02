65d0204fe57f31af831dcba419565fcd6329b8ab
sycatle <sycatle@pm.me>
2023-05-03 01:02:40 +0200

    changed index.css to common style.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./index.css";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
