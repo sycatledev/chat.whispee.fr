86b2b9cdb03759a2baec00986b50f6448e74c6c4
sycatle <sycatle@pm.me>
2023-05-03 15:43:16 +0200

    re-organized avatar component

--- client/src/components/user/Avatar.jsx
+++ client/src/components/user/Avatar.jsx
@@ -2,9 +2,9 @@ export default function Avatar({ username, size, color }) {
   return (
     <div
       className={`flex items-center justify-center uppercase font-normal select-none
+      text-white rounded-full cursor-pointer hover:shadow group-hover:shadow
       ${color || "bg-indigo-400"}
-      ${size ? `h-${size} w-${size}` : "h-8 w-8"} 
-      text-white rounded-full cursor-pointer hover:shadow group-hover:shadow`}
+      ${size ? `h-${size} w-${size}` : "h-8 w-8"}`}
     >
       {username[0]}
     </div>
