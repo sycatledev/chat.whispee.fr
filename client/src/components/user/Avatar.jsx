c80d8496beb7a32ce931d481aa946e9318615d25
sycatle <sycatle@pm.me>
2023-05-03 15:08:02 +0200

    removed parent formatting to avatar letter

--- client/src/components/user/Avatar.jsx
+++ client/src/components/user/Avatar.jsx
@@ -1,7 +1,7 @@
 export default function Avatar({ username, size, color }) {
   return (
     <div
-      className={`flex items-center justify-center uppercase select-none
+      className={`flex items-center justify-center uppercase font-normal select-none
       ${color || "bg-indigo-400"}
       ${size ? `h-${size} w-${size}` : "h-8 w-8"} 
       text-white rounded-full cursor-pointer hover:shadow group-hover:shadow`}
